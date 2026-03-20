52ec89685b2a3d13e5e57d48e1c1a6bdb3565b3d
ozneroL541 <l.21radice@gmail.com>
2026-03-20 14:23:37 +0100

    fix: update mcp2515 CAN overlay for can0 on spi0.0 and add overlay generation script

--- rasp-setup/mcp2515-overlays/overlay_maker.sh
+++ rasp-setup/mcp2515-overlays/overlay_maker.sh
@@ -1,5 +1,17 @@
+#!/bin/sh
+# This script is used to create the overlay files for the MCP2515 CAN controller.
+# It takes as input the spi and the interrupt pins and generates the overlay files accordingly.
+
+can_interface=can0
+spi_num=0
+chip_select=0
+spi="spi${spi_num}.${chip_select}"
+interrupt=gpio25
+filename="mcp2515-${can_interface}-overlay.dts"
+
+file_content="
 /*
- * Device tree overlay for mcp251x/can0 on spi0.0
+ * Device tree overlay for mcp251x/${can_interface} on ${spi}
  */
 
 /dts-v1/;
@@ -7,16 +19,16 @@
 
 / {
     compatible = "brcm,bcm2711";
-    /* disable spi-dev for spi0.0 */
+    /* disable spi-dev for ${spi} */
     fragment@0 {
-        target = <&spi0>;
+        target = <&spi${spi_num}>;
         __overlay__ {
             status = "okay";
         };
     };
 
     fragment@1 {
-	target = <&spidev0>;
+	target = <&spidev${spi_num}>;
 	__overlay__ {
 	    status = "disabled";
 	};
@@ -26,7 +38,7 @@
     fragment@2 {
         target = <&gpio>;
         __overlay__ {
-            can0_pins: can0_pins {
+            ${can_interface}_pins: ${can_interface}_pins {
                 brcm,pins = <25>; /* default pin, it will be overriden */
                 brcm,function = <0>; /* input */
             };
@@ -37,37 +49,39 @@
     fragment@3 {
         target-path = "/";
         __overlay__ {
-            /* external oscillator of mcp2515 on SPI0.0 */
-            can0_osc: can0_osc {
+            /* external oscillator of mcp2515 on ${spi} */
+            ${can_interface}_osc: ${can_interface}_osc {
                 compatible = "fixed-clock";
                 #clock-cells = <0>;
-                clock-frequency  = <16000000>; /* default ext clock speed, it will be overriden" */
+                clock-frequency  = <16000000>; /* default ext clock speed, it will be overriden" '*/
             };
         };
     };
 
     /* the spi config of the can-controller itself binding everything together */
     fragment@4 {
-        target = <&spi0>;
+        target = <&spi${spi_num}>;
         __overlay__ {
             /* needed to avoid dtc warning */
             #address-cells = <1>;
             #size-cells = <0>;
-            can0: mcp2515@0 {
+            ${can_interface}: mcp2515@0 {
                 reg = <0>;
                 compatible = "microchip,mcp2515";
                 pinctrl-names = "default";
-                pinctrl-0 = <&can0_pins>;
+                pinctrl-0 = <&${can_interface}_pins>;
                 spi-max-frequency = <10000000>;
                 interrupt-parent = <&gpio>;
                 interrupts = <25 8>; /* IRQ_TYPE_LEVEL_LOW */
-                clocks = <&can0_osc>;
+                clocks = <&${can_interface}_osc>;
             };
         };
     };
     __overrides__ {
-        oscillator = <&can0_osc>,"clock-frequency:0";
-        spimaxfrequency = <&can0>,"spi-max-frequency:0";
-        interrupt = <&can0_pins>,"brcm,pins:0",<&can0>,"interrupts:0";
+        oscillator = <&${can_interface}_osc>,"clock-frequency:0";
+        spimaxfrequency = <&${can_interface}>,"spi-max-frequency:0";
+        interrupt = <&${can_interface}_pins>,"brcm,pins:0",<&${can_interface}>,"interrupts:0";
     };
 };
+"
+
